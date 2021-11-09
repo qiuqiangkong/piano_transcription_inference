d4fe92e5016ddf941aee51fe3c47149344fe1a7b
QiuqiangKong <qiuqiangkong@gmail.com>
2021-11-09 11:13:55 +0800

    v0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="piano-transcription-inference", # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="Qiuqiang Kong",
     author_email="qiuqiangkong@gmail.com",
     description="Piano transcription inference toolbox",
